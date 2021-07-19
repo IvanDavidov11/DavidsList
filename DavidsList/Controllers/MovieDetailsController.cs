3c70d09af788ec0bc897ee3d8d9f5bd266e9c203
ivan <vanuhata@abv.bg>
2021-07-20 02:19:22 +0300

    Simple changes and fixes to design. Added basic  details page functionality, needs styling.

---
+++ DavidsList/Controllers/MovieDetailsController.cs
@@ -0,0 +1,52 @@
+﻿namespace DavidsList.Controllers
+{
+    using DavidsList.Models.ViewModels;
+    using Microsoft.AspNetCore.Mvc;
+    using Newtonsoft.Json;
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    public class MovieDetailsController : Controller
+    {
+        public IActionResult Index(string id)
+        {
+            var model =  GetMovieDetailsFromApi(id).Result;
+            return View(model);
+        }
+
+        private static async Task<MovieDetailsViewModel> GetMovieDetailsFromApi(string moviePath)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://imdb8.p.rapidapi.com/title/get-overview-details?tconst={moviePath}&currentCountry=US"),
+                Headers =
+                 {
+                     { "x-rapidapi-key", "6e96b86691mshebfd322284464b6p1c6f16jsnc9e27395f9df" },
+                     { "x-rapidapi-host", "imdb8.p.rapidapi.com" },
+                 },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var bodyJson = JsonConvert.DeserializeObject<MovieDetailsApiModel>(body);
+                return new MovieDetailsViewModel
+                {
+                    Title = bodyJson.title.title,
+                    ImgUrl= bodyJson.title.image.url,
+                    MoviePath = moviePath,
+                    RunningTimeInMinutes = bodyJson.title.runningTimeInMinutes,
+                    ReleaseDate = bodyJson.releaseDate,
+                    Raiting = bodyJson.ratings.rating,
+                    RaitingCount = bodyJson.ratings.ratingCount,
+                    Genres = bodyJson.genres,
+                    ShortPlot = bodyJson.plotOutline.text,
+                    LongPlot = bodyJson.plotSummary.text,
+                };
+            }
+        }
+    }
+}
---
+++ DavidsList/Models/ViewModels/MovieDetailsViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace DavidsList.Models.ViewModels
+{
+    using System.Collections.Generic;
+    public class MovieDetailsViewModel
+    {
+        public string Title { get; set; }
+        public string ImgUrl { get; set; }
+        public string MoviePath { get; set; }
+        public int RunningTimeInMinutes { get; set; }
+        public string ReleaseDate { get; set; }
+
+        public double Raiting { get; set; }
+        public int RaitingCount { get; set; }
+
+        public List<string> Genres { get; set; }
+
+        public string ShortPlot { get; set; }
+        public string LongPlot { get; set; }
+    }
+}
